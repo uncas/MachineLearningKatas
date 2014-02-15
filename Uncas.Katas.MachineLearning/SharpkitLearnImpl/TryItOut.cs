f6c155b709d0d1e438925326fe6f0d804037510b
Ole Lynge Soerensen <olelynge@gmail.com>
2014-02-15 14:18:43 +0100

    SharpKit.Learn: Retrieve probability from logistic regression.

--- Uncas.Katas.MachineLearning/SharpkitLearnImpl/TryItOut.cs
+++ Uncas.Katas.MachineLearning/SharpkitLearnImpl/TryItOut.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Numerics.LinearAlgebra.Generic;
 using NUnit.Framework;
 using Sharpkit.Learn.LinearModel;
 
@@ -7,32 +9,41 @@ namespace Uncas.Katas.MachineLearning.SharpkitLearnImpl
     [TestFixture]
     public class TryItOut
     {
-        [Test]
-        public void RidgeClassifier_Ok()
+        private static LogisticRegression<bool> GetLogisticRegression(double[,] samples,
+            bool[] knownOutput)
         {
-            var clf = new RidgeClassifier<string>(0.5);
-            double[,] samples = {{1.0, 3.0}, {3.0, 5.0}, {1.0, 0.0}, {10.0, 1.0}};
-            const string nonFraud = "non-fraud";
-            const string fraud = "fraud";
-            string[] knownOutput = {fraud, fraud, nonFraud, nonFraud};
-            clf.Fit(samples, knownOutput);
-            Console.WriteLine(clf.Coef);
-            Console.WriteLine(clf.Intercept);
-
-            for (int age = 1; age < 30; age++)
-                for (int words = 0; words < 10; words++)
-                {
-                    string prediction = clf.Predict(new double[] {age, words});
-                    Console.WriteLine("{0} days, {1} words, {2}", age, words, prediction);
-                }
+            var clf = new LogisticRegression<bool>();
+            clf.Fit(DenseMatrix.OfArray(samples), knownOutput);
+            return clf;
         }
 
         [Test]
-        public void X()
+        public void LogisticRegression_Ok()
         {
-            //var regression = new LogisticRegression<string>();
-            //regression.Fit();
-            //regression.PredictProba()
+            double[,] samples = {{1.0, 0.0}, {10.0, 1.0}, {1.0, 3.0}, {3.0, 5.0}};
+            bool[] knownOutput = {false, false, true, true};
+            LogisticRegression<bool> clf = GetLogisticRegression(samples, knownOutput);
+            Console.WriteLine(clf.Coef);
+            Console.WriteLine(clf.Intercept);
+            for (int age = 1; age < 30; age += 2)
+                for (int words = 0; words < 10; words += 2)
+                {
+                    double[] sample = {age, words};
+                    bool prediction = clf.Predict(sample);
+                    Console.WriteLine("{0} days, {1} words, {2}", age, words,
+                        prediction);
+
+                    double[,] localSample = {{age, words}};
+                    Matrix<double> proba =
+                        clf.PredictProba(DenseMatrix.OfArray(localSample));
+                    foreach (double prob in proba.ToArray())
+                    {
+                        Console.WriteLine("Prob {0}", prob);
+                    }
+
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
         }
     }
 }
